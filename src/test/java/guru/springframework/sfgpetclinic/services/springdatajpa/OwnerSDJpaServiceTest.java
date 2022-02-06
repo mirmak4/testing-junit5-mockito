e49194ca8c04339fd4de535ee77f3a61fe3c5ae9
miron.maksymiuk <miron.maksymiuk@OSZ1-LDL-P91358.synapse.com>
2022-02-06 02:26:59 +0100

    Added owner service findByLastName test.

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -1,25 +1,41 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
 
-@Disabled(value = "Disabled until we learn Mocking")
+//@Disabled(value = "Disabled until we learn Mocking")
 class OwnerSDJpaServiceTest {
 
-    OwnerSDJpaService service;
+    @InjectMocks
+    private OwnerSDJpaService service;
+    @Mock
+    private OwnerRepository repository;
 
     @BeforeEach
     void setUp() {
-        service = new OwnerSDJpaService(null, null, null);
-
+        MockitoAnnotations.initMocks(this);
     }
 
-    @Disabled
+//    @Disabled
     @Test
-    void findByLastName() {
+    public void findByLastName() {
+        Owner owner = new Owner(1L, "Fred", "Mercury");
+        when(repository.findByLastName(anyString())).thenReturn(owner);
         Owner foundOwner = service.findByLastName("Buck");
+        verify(repository).findByLastName(anyString());
+        assertNotNull(foundOwner);
     }
 
     @Test
